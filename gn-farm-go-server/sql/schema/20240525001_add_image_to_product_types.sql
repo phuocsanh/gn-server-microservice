c4cfbdd97545af00bdf7d25ab3ade1f9a75b1b15
phuocsanh <phuocsanhtps@gmail.com>
2025-05-28 15:07:33 +0700

    up

--- gn-farm-go-server/sql/schema/20240525001_add_image_to_product_types.sql
+++ gn-farm-go-server/sql/schema/20240525001_add_image_to_product_types.sql
@@ -2,10 +2,10 @@
 -- Thêm trường image_url vào bảng product_types
 ALTER TABLE product_types ADD COLUMN image_url TEXT;
 
--- Cập nhật dữ liệu mẫu với ảnh cho các product types hiện có
-UPDATE product_types SET image_url = 'https://example.com/images/mushroom-category.jpg' WHERE name = 'Mushroom';
-UPDATE product_types SET image_url = 'https://example.com/images/vegetable-category.jpg' WHERE name = 'Vegetable';
-UPDATE product_types SET image_url = 'https://example.com/images/bonsai-category.jpg' WHERE name LIKE '%Bonsai%';
+-- Cập nhật dữ liệu mẫu với ảnh thật cho các product types hiện có
+UPDATE product_types SET image_url = 'https://images.unsplash.com/photo-1578662996442-48f60103fc96?w=500' WHERE name = 'Mushroom';
+UPDATE product_types SET image_url = 'https://images.unsplash.com/photo-1540420773420-3366772f4999?w=500' WHERE name = 'Vegetable';
+UPDATE product_types SET image_url = 'https://images.unsplash.com/photo-1416879595882-3373a0480b5b?w=500' WHERE name LIKE '%Bonsai%';
 
 -- +goose Down
 -- Xóa trường image_url khỏi bảng product_types
